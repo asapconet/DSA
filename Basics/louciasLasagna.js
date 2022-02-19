53733e80f07037353404765a955306dbde08f87c
asapconet <aaronrickymeek@gmail.com>
2022-02-19 22:19:12 +0100

    Nano Updata: louicias file update

---
+++ Basics/louciasLasagna.js
@@ -0,0 +1,8 @@
+const totalOvenTime = 40;
+
+function remainingMinutesInOven() {
+    timeLeft = totalOvenTime - 30
+    return (
+        timeLeft
+    )
+}
--- Basics/two-fer.js
+++ Basics/two-fer.js
@@ -3,3 +3,5 @@ const twoFer = (name = 'jon') => {
         'One for' + {name}, + 'One for me'
     )
 }
+
+twoFer()
