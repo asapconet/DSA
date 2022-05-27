a411e4849196c62451f7cacd31dbcc1c3ed22c2a
asapconet <aaronrickymeek@gmail.com>
2022-05-27 15:45:46 +0100

    Buggy: quicksort DS implementation

--- SearchnSort/quickSort.js
+++ SearchnSort/quickSort.js
@@ -1,3 +1,4 @@
+// THE INTERNALS OF QUICK_SORT ALGO
 // This function sorts an array, in form of 'qsort' format
 
 const quickSort = (array) => {
@@ -5,7 +6,7 @@ const quickSort = (array) => {
     return array; // when base case condition is archived
   } else {
     let pivot = array[0]; // in recursive case, index can be any location.
-    // let less =  [i for i in array[1:] if(i <= pivot) ] (phuedocode for LHS of the pivot)
+    // let less =  [i for i in array[1:] if(i <= pivot) ] (pheudo for LHS of the pivot)
     // uncertain js logic
     for (let i = array[i]; i <= pivot; i++) {
       let less = i;
@@ -17,3 +18,7 @@ const quickSort = (array) => {
     return quickSort(less) + pivot + quickSort(greater);
   }
 };
+console.log(quickSort([2, 5, 33, 8, 4, 1]));
+
+
+// Every language have it's dedicated inbuilt function for the act
